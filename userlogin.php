293f74bd0c3a594eca187af7bed11f62525be160
Anmol Wassan <wassan.anmol@gmail.com>
2016-09-07 20:01:05 +0530

    Updated with MySQLi OOP implementation.

--- userlogin.php
+++ userlogin.php
@@ -5,38 +5,45 @@ session_start();
 if (isset($_SESSION['username']) && isset($_SESSION['password']))
 {
     header('Location: index.php');
+    exit;
 } else
 {
+    $pass = '';
     require 'connecting to database.php';
     require 'login.html';
     //The main query function
     function sendQuery()
     {
+        global $connect_db;
         if (isset($_POST['username']) && isset($_POST['password']))
         {
             $username = htmlentities($_POST['username']); //Using htmlentities() function to render HTML as text.
             $password = htmlentities($_POST['password']);
         }
-        $query_login = 'SELECT `username` , `password` FROM `users` WHERE `username`="' .
-            mysql_real_escape_string($username).'"'; //Using mysql_real_escape_string function to prevent SQL Injection attacks.
-        if ($query = mysql_query($query_login))
+        if ($query_login = $connect_db->prepare('SELECT `username` , `password` FROM `users` WHERE `username`=?'))
         {
-            if(mysql_num_rows($query)==0)            //If the query returns 0 rows, i.e, no data, then print error message.
+            $query_login->bind_param('s', $username);
+            if ($query_login->execute())
             {
-                echo 'Invalid username/password';
-            }
-            while ($query_run = mysql_fetch_assoc($query))  
-            {
-                if (password_verify($password, $query_run['password'])) //Checks the hash for the password
+                $numrows=$query_login->num_rows;
+                if($numrows==0)
+                    {
+                        echo 'Invalid username/password. <br />';
+                    }
+                $query_login->bind_result($username, $pass);
+                while ($query_login->fetch())
                 {
-                    $_SESSION['username'] = $username;
-                    $_SESSION['password'] = $password;
-                    header('Location: index.php');
-                    exit;
-                } else
-                {
-                    echo 'Invalid username/password.';
-                    session_destroy();
+                    if(password_verify($password, $pass))
+                    {
+                        $_SESSION['username']=$username;
+                        $_SESSION['password']=$password;
+                        header('Location: index.php');
+                        exit;
+                    } else if($numrows!=0 && !password_verify($password, $pass))
+                    {
+                        echo 'Invalid username/password. <br />';
+                        session_destroy();
+                    }
                 }
             }
         }
